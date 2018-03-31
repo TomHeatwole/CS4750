6bddd33d8da0e69a7831fed05105ff382b8d09fc
tomheatwole <tgh8xn@virginia.edu>
2018-03-31 17:38:30 -0400

    Display league name on /league?id=id

--- league/index.php
+++ league/index.php
@@ -7,6 +7,15 @@
         include("../config.php");
         if (!isset($_GET['id'])) header("Location: ../leagues/");
         $leagueId = $_GET['id'];
+        $conn = mysqli_connect($host, $username, $password, $database);
+        $leagueName = $conn->query("SELECT name FROM League WHERE league_id=" . $leagueId);
+        $ln = $leagueName->fetch_assoc();
+        if (!$ln) {
+            echo "<h1>League not found</h1>";
+            echo "<a href='../leagues'>List of leagues</a>";
+            exit();
+        }
+        echo "<h1>". $ln['name'] . "</h1>";
     ?>
     <table>
         <!-- TODO: League data -->
