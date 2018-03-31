2291cfdabccebf40731ce1ff6c42f37133e7d639
tomheatwole <tgh8xn@virginia.edu>
2018-03-31 16:09:39 -0400

    Example table built from SQL SELECT statement

--- views/leagues.php
+++ views/leagues.php
@@ -1,6 +1,25 @@
-<?php 
-    include('../setup.php');
-    $conn = mysqli_connect($host, $username, $password, $database);
-    $result = $conn->query("SELECT * FROM User");
-    echo $result->fetch_assoc()['username'];
-?>
+<head>
+    <link rel="stylesheet" href="../app.css">
+</head>
+<body>
+    <h1> Leagues </h1>
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Admin</th>
+            <th># of Players</th>
+        <tr>
+        <?php
+            include('../setup.php'); #This file is in .gitignore
+            $conn = mysqli_connect($host, $username, $password, $database);
+            $result = $conn->query("SELECT * FROM League");
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>" . $row['name'] . "</td>";
+                echo "<td>" . "placeholder" . "</td>";
+                echo "<td>" . "placeholder" . "</td>";
+                echo "</tr>";
+            }
+        ?>
+    </table>
+</body>
