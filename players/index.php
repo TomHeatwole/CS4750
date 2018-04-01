69faa604a7b5a36c12244e420ae2136ad3907cc7
Stephen Read <stephenrva@gmail.com>
2018-03-31 21:43:36 -0400

    players

--- players/index.php
+++ players/index.php
@@ -13,7 +13,7 @@
         <?php
             include('../database.php'); #This file is in .gitignore
             $conn = mysqli_connect($host, $username, $password, $database);
-            $result = $conn->query("SELECT * FROM Player");
+            $result = $conn->query("SELECT * FROM Player ORDER BY elo");
             while ($row = $result->fetch_assoc()) {
                 echo "<tr>";
                 echo "<td><a href='../player?username=" . $row['username'] . "'>" . $row['username'] . "</a></td>";
