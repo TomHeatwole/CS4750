07acda9b2e9b412604e14a2113cf54e01ac09e0b
Anchit Kolla <anchitkolla@yahoo.com>
2018-04-14 18:25:34 -0400

    Created a homepage

---
+++ index.php
@@ -0,0 +1,14 @@
+<head>
+    <link rel="stylesheet" href="../app.css">
+</head>
+<body>
+    <?php
+        include('database.php'); #This file is in .gitignore
+        include("config.php");
+        echo "<h1>Elo League Tracker</h1>";
+        echo "<h2><a href = '/leagues'>All Leagues</a></h2>";
+        echo "<h2><a href = '/players'>All Players</a></h2>";
+        echo "<h2><a href = '/gamereport'>Game Report</a></h2>";
+
+    ?>
+</body>
