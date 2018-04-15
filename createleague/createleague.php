2e5e97c42cc6f3d14c2ea6415426a1ec5c5df707
tomheatwole <tgh8xn@virginia.edu>
2018-04-14 22:14:29 -0400

    Create League adds a row to Moderates table as well

--- createleague/createleague.php
+++ createleague/createleague.php
@@ -1,31 +1,21 @@
 <?php
- include("../database.php"); // To connect to the database
- include("../config.php");
- $con = mysqli_connect($host, $username, $password, $database);
- // Check connection
- if (mysqli_connect_errno())
- {
- echo "Failed to connect to MySQL: " . mysqli_connect_error();
- }
- // Form the SQL query (an INSERT query)
- $LeagueId =("SELECT league_id FROM League");
- $result = $con->query($LeagueId);
- $max = 0;
- while($row = $result->fetch_assoc()){
- 	if($row[league_id] > $max){
- 		$max = $row[league_id];
- 	}
- }
- $new = $max + 1;
- $name = "INSERT INTO League(league_id, name) VALUES ('$new', '$_POST[name]')";
- $Admin="INSERT INTO Admin (username, phone_number)
- VALUES
- ('$_POST[username]','$_POST[phone_number]')";
+include("../database.php"); // To connect to the database
+include("../config.php");
+$conn = mysqli_connect($host, $username, $password, $database);
+// Check connection
+if (mysqli_connect_errno())
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+// Form the SQL query (an INSERT query)
+$conn->query("INSERT INTO League(name) VALUES ('$_POST[name]')");
+$leagueId = $conn->query("SELECT league_id FROM League ORDER BY league_id DESC");
+$id = $leagueId->fetch_assoc()["league_id"];
+$Admin="INSERT INTO Admin (username, phone_number)
+    VALUES
+    ('$_POST[username]','$_POST[phone_number]')";
+$conn->query("INSERT INTO Moderates (username, league_id) VALUES ('$_POST[name]', '$id')");
 
- if (!mysqli_query($con,$name) || !mysqli_query($con,$LeagueId) || !mysqli_query($con,$Admin))
- {
- die('Error: ' . mysqli_error($con));
- }
- echo "Records added"; // Output to user
- mysqli_close($con);
+echo "Records added"; // Output to user
+mysqli_close($con);
 ?> 
