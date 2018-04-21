806a3e3a1a16d3496ffa0225e22d694b064ab85f
Stephen Read <stephenrva@gmail.com>
2018-04-21 14:26:46 -0400

    added validation

--- belongsto/BelongsToInsert.php
+++ belongsto/BelongsToInsert.php
@@ -5,9 +5,14 @@
     if (mysqli_connect_errno()) {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
-    $conn->query("INSERT INTO Requests (username, league_id) VALUE ('$_POST[username]','$_POST[leagueID]')");
-    $conn->query("INSERT INTO LeagueRecord (username, league_id) VALUE ('$_POST[username]','$_POST[leagueID]')");
-
-    echo "Request made"; // Output to user
-    mysqli_close($con);
+    $check = $conn->query("SELECT * FROM BelongsTo WHERE username = '$_POST[username]' AND league_id = '$_POST[leagueID]'");
+    $test = $check->fetch_array();
+    if(!$test){
+        $conn->query("INSERT INTO Requests (username, league_id) VALUE ('$_POST[username]','$_POST[leagueID]')");
+        $conn->query("INSERT INTO LeagueRecord (username, league_id) VALUE ('$_POST[username]','$_POST[leagueID]')");
+        echo "Request made"; // Output to user
+    } else{
+        echo "Already exists";
+    }
+    mysqli_close($conn);
 ?> 
