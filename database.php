9878a63983451ebca1a93777504612112c335007
TomHeatwole <tgh8xn@virginia.edu>
2018-04-24 22:31:55 -0400

    Fixing filepath on not logged in redirect

--- database.php
+++ database.php
@@ -43,7 +43,7 @@
 
 session_start();
 $type = $_SESSION["usertype"];
-if (!$type) echo "<script>window.location='./auth'</script>";
+if (!$type) echo "<script>window.location='/auth'</script>";
 $username = ($type == "god") ? "CS4750tgh8xna" : "CS4750tgh8xnc" ;
 if ($type == "admin") $username = "CS4750tgh8xnb";
 $host = "stardock.cs.virginia.edu";
