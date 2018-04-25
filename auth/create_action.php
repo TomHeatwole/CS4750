0c216aa5b9b79c97466741c45fb5c85dfb27c417
Anchit Kolla <anchitkolla@yahoo.com>
2018-04-25 04:00:20 -0400

    Made changes for trigger #1

--- auth/create_action.php
+++ auth/create_action.php
@@ -16,8 +16,6 @@
         $conn = mysqli_connect($host, $username, $password, $database);
         $result = $conn->query("INSERT INTO User (username, password, first_name, last_name, email)
         VALUES (" . "'" . $usrname . "'" . "," . "'" . $hash . "'" . ",'" . $fname . "','" . $lname . "','" . $email . "'" . ")");       
-        $another = $conn->query("INSERT INTO Player (username, elo, wins, losses)
-        VALUES (" . "'" . $usrname . "', 1200, 0, 0)");
 
         $admin = $conn->query("SELECT * FROM Admin WHERE username = '" . $usrname . "'");
 
@@ -38,9 +36,9 @@
                 $_SESSION["usertype"] = "normie";
             }
             echo "Made account";
-            /*echo "<script>
+            echo "<script>
                 window.location = '../index.php';
-           </script>";*/
+           </script>";
         }
 
         else{
