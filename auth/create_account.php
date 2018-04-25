db49f616439af1a605c20b4b1e5fba0e17fd09bd
TomHeatwole <tgh8xn@virginia.edu>
2018-04-24 22:55:39 -0400

    Make form in createaccount not look like garbage

--- auth/create_account.php
+++ auth/create_account.php
@@ -1,8 +1,7 @@
 <body>
-    <?php
-        include('auth_database.php');
-        include("../config.php");
-    ?>
+<?php
+include('auth_database.php');
+?>
     <script>document.getElementById("home").classList.add("active");</script>
     <div id="inner" style="margin-left: 35%; text-align: left">
 
@@ -11,22 +10,35 @@
     <br>
 
     <form method="POST" action='/auth/create_action.php'>
-        <label><b>First Name: </b></label>
-        <input type="text" placeholder="Enter First Name" name="fname" id="fname" required>
-        <br>
-        <label><b>Last Name: </b></label>
-        <input type="text" placeholder="Enter Last Name" name="lname" id="lname" required>
-        <br>
-        <label><b>Email: </b></label>
-        <input type="text" placeholder="Enter Email" name="email" id="email" required>
-        <br>
-        <label><b>Username: </b></label>
-        <input type="text" placeholder="Enter Username" name="uname" id="uname" required>
-        <br>
-        <label><b>Password: </b></label>
-        <input type="password" placeholder="Enter Password" name="psw" id="uname" required>
+        <div style="display: table"><div style="display: table-row; line-height: 30px">
+            <div style="display: table-cell">
+                <label><b>First Name: </b></label>
+                <br>
+                <label><b>Last Name: </b></label>
+                <br>
+                <label><b>Email: </b></label>
+                <br>
+                <label><b>Username: </b></label>
+                <br>
+                <label><b>Password: </b></label>
+                <br>
+            </div>
+            <div style="display: table-cell">
+                <input type="text" placeholder="Enter First Name" name="fname" id="fname" required>
+                <br>
+                <input type="text" placeholder="Enter Last Name" name="lname" id="lname" required>
+                <br>
+                <input type="text" placeholder="Enter Email" name="email" id="email" required>
+                <br>
+                <input type="text" placeholder="Enter Username" name="uname" id="uname" required>
+                <br>
+                <input type="password" placeholder="Enter Password" name="psw" id="uname" required>
+                <br>
+            </div>
+        </div></div>
         <br>
         <input type="submit">
+        <br>
     </form>
 
     <br>
