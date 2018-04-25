a7ef76cd96d1bb48c1a0fa73391dfa1211a08e78
TomHeatwole <tgh8xn@virginia.edu>
2018-04-24 23:33:03 -0400

    Make directory for export function

---
+++ data/index.php
@@ -0,0 +1,6 @@
+<?php
+session_start();
+include('database.php'); #This file is in .gitignore
+?>
+<div id="inner" style="margin-left: 35%; text-align: left">
+<h2> Please wait - the data is being prepared </h2>
