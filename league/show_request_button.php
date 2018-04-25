6c39cb654695bdc1a628138f537700d1d5938394
TomHeatwole <tgh8xn@virginia.edu>
2018-04-25 03:56:06 -0400

    Fixed bugs in requests workflow

--- league/show_request_button.php
+++ league/show_request_button.php
@@ -6,8 +6,9 @@
 include("../database_only.php");
 
 $ret = "pass";
+echo $name;
 $id = $_POST["id"];
-$res1 = $conn->query("SELECT username FROM SeasonRecord WHERE league_id='$id' AND username='$name'");
+$res1 = $conn->query("SELECT username FROM LeagueRecord WHERE league_id='$id' AND username='$name'");
 $res2 = $conn->query("SELECT username FROM Requests WHERE league_id='$id' AND username='$name'");
 if ($res1->fetch_assoc() || $res2->fetch_assoc()) $ret = "false";
 echo $ret;
